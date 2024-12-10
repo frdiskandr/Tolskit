730a7ae495bd06642ba1c7ec2ce3e1a06744b6e6
Farid iskandar <fariditb159@gmailcom>
2024-12-10 18:30:59 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <nav className='navbar'>
+     <ul className='element-nav'>
+      <li><a href="#home">Home</a></li>
+      <li>Tools</li>
+      <li>About</li>
+     </ul>
+      </nav>
+    </>
+  )
+}
+
+export default App
